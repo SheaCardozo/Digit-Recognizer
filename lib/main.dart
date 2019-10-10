93bde72aa5043c4a128133cd736d364b2a195a6d
SheaCardozo <sacardoz@uwaterloo.ca>
2019-10-09 22:06:31 -0400

    drawing overhaul + image capture

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+import 'package:flutter/rendering.dart';
+import 'dart:io';
 
 void main() => runApp(MyApp());
 
@@ -44,29 +47,32 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<DrawingPoints> points = List();
+  Color selectedColor = Colors.black;
+  Color pickerColor = Colors.black;
+  bool showBottomList = false;
+  StrokeCap strokeCap = StrokeCap.butt;
+  SelectedMode selectedMode = SelectedMode.StrokeWidth;
+  GlobalKey _drawKey = GlobalKey();
+  ui.Image shot;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void _captureDigit() async {
+    RenderRepaintBoundary boundary = _drawKey.currentContext.findRenderObject();
+    shot = await boundary.toImage();
   }
+
+  void _clear() {
+    setState(() {
+      points.clear();
+    });  }
 
   @override
   Widget build(BuildContext context) {
     final screenHeight = (MediaQuery.of(context).size.height - kToolbarHeight - MediaQuery.of(context).padding.top);
     final screenWidth = MediaQuery.of(context).size.width;
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    double strokeWidth = screenWidth * 20.0/420.0;
+    double opacity = 1.0;
+
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -77,53 +83,65 @@ class _MyHomePageState extends State<MyHomePage> {
       Column(
         children: <Widget>[
         Container(
-
+        key: _drawKey,
         alignment: Alignment.topCenter,
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
 
         constraints: BoxConstraints.expand(
-          height: screenHeight * 0.8,
+          height: screenWidth,
         ),
 
-        color: Colors.red,
+        child: GestureDetector(
+          behavior: HitTestBehavior.translucent,
 
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          onPanUpdate: (details) {
+            setState(() {
+              RenderBox rb = _drawKey.currentContext.findRenderObject();
+              points.add(DrawingPoints(
+                  points: rb.globalToLocal(details.globalPosition),
+                  paint: Paint()
+                    ..strokeCap = strokeCap
+                    ..isAntiAlias = true
+                    ..color = selectedColor.withOpacity(opacity)
+                    ..strokeWidth = strokeWidth));
+            });
+            _captureDigit();
+          },
+          onPanStart: (details) {
+            setState(() {
+              RenderBox rb = _drawKey.currentContext.findRenderObject();
+              points.add(DrawingPoints(
+                  points: rb.globalToLocal(details.globalPosition),
+                  paint: Paint()
+                    ..strokeCap = strokeCap
+                    ..isAntiAlias = true
+                    ..color = selectedColor.withOpacity(opacity)
+                    ..strokeWidth = strokeWidth));
+              _captureDigit();
+            });
+          },
+          onPanEnd: (details) {
+            setState(() {
+              points.add(null);
+              _captureDigit();
+            });
+          },
+          child: CustomPaint(
+            size: Size.square(screenWidth),
+            painter: DrawingPainter(
+              pointsList: points,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+          ),
         ),
-      ),
+        ),
         Row(
           children: <Widget>[
         Container(
           alignment: Alignment.bottomLeft,
-          // Center is a layout widget. It takes a single child and positions it
-          // in the middle of the parent.
 
           constraints: BoxConstraints.expand(
-            height: screenHeight * 0.2,
+            height: screenHeight - screenWidth,
             width: screenWidth/2,
           ),
 
@@ -135,7 +153,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // in the middle of the parent.
 
           constraints: BoxConstraints.expand(
-            height: screenHeight * 0.2,
+            height: screenHeight - screenWidth,
             width: screenWidth/2,
           ),
 
@@ -146,10 +164,43 @@ class _MyHomePageState extends State<MyHomePage> {
       ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+        onPressed: _clear,
+        tooltip: 'Clear',
+        child: Icon(Icons.clear),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+class DrawingPainter extends CustomPainter {
+  DrawingPainter({this.pointsList});
+  List<DrawingPoints> pointsList;
+  List<Offset> offsetPoints = List();
+  @override
+  void paint(Canvas canvas, Size size) {
+    for (int i = 0; i < pointsList.length - 1; i++) {
+      if (pointsList[i] != null && pointsList[i+1] != null) {
+        int steps = (pointsList[i].points - pointsList[i+1].points).distance ~/ (pointsList[i].paint.strokeWidth / 10.0);
+        for (int j = 0; j < steps; j++){
+          canvas.drawCircle(pointsList[i].points - (pointsList[i].points - pointsList[i+1].points) * j.toDouble() / steps.toDouble(), pointsList[i].paint.strokeWidth,
+              pointsList[i].paint);
+        }
+      } else if (pointsList[i] != null && pointsList[i + 1] == null) {
+        canvas.drawCircle(pointsList[i].points, pointsList[i].paint.strokeWidth,
+            pointsList[i].paint);
+      }
+    }
+  }
+
+  @override
+  bool shouldRepaint(DrawingPainter oldDelegate) => true;
+}
+
+
+class DrawingPoints {
+  Paint paint;
+  Offset points;
+  DrawingPoints({this.points, this.paint});
+}
+
+enum SelectedMode { StrokeWidth, Opacity, Color }
